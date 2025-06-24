d426b975a5f4c29faa0fc67e09c3f87d426c9a79
baileytorch <baileytorch@hotmail.com>
2025-06-24 16:38:15 -0400

    Actuializando DDL

--- manejo_notas/data/gestion_notas_DDL.sql
+++ manejo_notas/data/gestion_notas_DDL.sql
@@ -2,18 +2,18 @@ USE gestion_notas;
 
 CREATE TABLE IF NOT EXISTS docentes(
     id INT NOT NULL AUTO_INCREMENT,
-    rut VARCHAR(12) NOT NULL,
-    nombre VARCHAR(250) NOT NULL,
-    email VARCHAR(250) NULL,
+    rut_docente VARCHAR(12) NOT NULL UNIQUE,
+    nombre_docente VARCHAR(250) NOT NULL,
+    email_docente VARCHAR(250) NULL,
 
     CONSTRAINT pk_docente PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS asignaturas(
     id INT NOT NULL AUTO_INCREMENT,
-    codigo VARCHAR(12) NOT NULL,
-    nombre VARCHAR(250) NOT NULL,
-    descripcion VARCHAR(250) NULL,
+    codigo_asignatura VARCHAR(12) NOT NULL,
+    nombre_asignatura VARCHAR(250) NOT NULL,
+    descripcion-asignatura VARCHAR(250) NULL,
 
     CONSTRAINT pk_asignaturas PRIMARY KEY (id)
 );
---
+++ manejo_notas/data/insert_data_DML.sql
@@ -0,0 +1,10 @@
+USE gestion_notas;
+
+INSERT INTO docentes (rut,nombre,email) VALUES 
+('12345678-5','Aquiles Baeza','aquiles.baeza@test.test'),
+('12345678-4','Wendy Sulca','wendysulca@perucito.pe');
+
+INSERT INTO asignaturas (codigo,nombre) VALUES
+('BIO','Biología'),
+('FIS','Física General'),
+('QUI','Química Inorgánica');
